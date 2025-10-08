43e5ad4db01d350ae2e48ac431d7afbf87ffa363
tanakornsss <omtanakorn2550@gmail.com>
2025-10-08 13:19:37 +0700

    Add basic layout for adding notes

--- app/src/main/java/dev/tanakornsss/luminality/ui/screen/LandingScreen.kt
+++ app/src/main/java/dev/tanakornsss/luminality/ui/screen/LandingScreen.kt
@@ -1,19 +1,61 @@
 package dev.tanakornsss.luminality.ui.screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices.PIXEL_9
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun LandingScreen() {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
+    var textFieldValue by remember { mutableStateOf("") }
 
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(modifier = Modifier
+            .padding(innerPadding)
+            .padding(horizontal = 16.dp)
+            .fillMaxSize()
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceAround,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                OutlinedTextField(
+                    value = textFieldValue,
+                    onValueChange = {
+                        textFieldValue = it
+                    },
+                    label = {
+                        Text("What's good today?")
+                    },
+                )
+                Button(
+                    onClick = { },
+                    modifier = Modifier.height(56.dp)
+                ) {
+                    Icon(Icons.Filled.Add, contentDescription = null)
+                }
+            }
         }
     }
 }
