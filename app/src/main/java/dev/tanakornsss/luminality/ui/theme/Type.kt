2cce3f3c4805666641d3eb846da4d2a380ae22fd
tanakornsss <omtanakorn2550@gmail.com>
2025-10-11 14:08:54 +0700

    Add new font

--- app/src/main/java/dev/tanakornsss/luminality/ui/theme/Type.kt
+++ app/src/main/java/dev/tanakornsss/luminality/ui/theme/Type.kt
@@ -1,34 +1,33 @@
 package dev.tanakornsss.luminality.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import dev.tanakornsss.luminality.R
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+
+val Sriracha = FontFamily(
+    Font(R.font.sriracha_regular, FontWeight.Normal)
 )
+
+val Typography = Typography().run {
+    copy(
+        displayLarge = displayLarge.copy(fontFamily = Sriracha),
+        displayMedium = displayMedium.copy(fontFamily = Sriracha),
+        displaySmall = displaySmall.copy(fontFamily = Sriracha),
+        headlineLarge = headlineLarge.copy(fontFamily = Sriracha),
+        headlineMedium = headlineMedium.copy(fontFamily = Sriracha),
+        headlineSmall = headlineSmall.copy(fontFamily = Sriracha),
+        titleLarge = titleLarge.copy(fontFamily = Sriracha),
+        titleMedium = titleMedium.copy(fontFamily = Sriracha),
+        titleSmall = titleSmall.copy(fontFamily = Sriracha),
+        bodyLarge = bodyLarge.copy(fontFamily = Sriracha),
+        bodyMedium = bodyMedium.copy(fontFamily = Sriracha),
+        bodySmall = bodySmall.copy(fontFamily = Sriracha),
+        labelLarge = labelLarge.copy(fontFamily = Sriracha),
+        labelMedium = labelMedium.copy(fontFamily = Sriracha),
+        labelSmall = labelSmall.copy(fontFamily = Sriracha)
+    )
+}
