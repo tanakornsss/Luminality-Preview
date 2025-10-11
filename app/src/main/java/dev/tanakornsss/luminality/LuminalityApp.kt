b9ecb174f1564907c04c62fe6e413cc2eedf0e2f
tanakornsss <omtanakorn2550@gmail.com>
2025-10-11 10:02:42 +0700

    Create ViewModel factory

--- app/src/main/java/dev/tanakornsss/luminality/LuminalityApp.kt
+++ app/src/main/java/dev/tanakornsss/luminality/LuminalityApp.kt
@@ -4,14 +4,17 @@ import android.content.Context
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.tanakornsss.luminality.data.JournalViewModel
+import dev.tanakornsss.luminality.data.JournalViewModelFactory
 import dev.tanakornsss.luminality.ui.screen.JournalScreen
 import dev.tanakornsss.luminality.ui.theme.LuminalityTheme
 
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
 fun LuminalityApp(context: Context) {
-    val journalViewModel = JournalViewModel(context)
+    val journalViewModel: JournalViewModel =
+        viewModel(factory = JournalViewModelFactory(context))
 
     LuminalityTheme {
         JournalScreen(journalViewModel, context)
---
+++ app/src/main/java/dev/tanakornsss/luminality/data/JournalViewModelFactory.kt
@@ -0,0 +1,17 @@
+package dev.tanakornsss.luminality.data
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class JournalViewModelFactory(
+    private val context: Context
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(JournalViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return JournalViewModel(context) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
