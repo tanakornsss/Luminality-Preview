a8450d9103d469b165ffa89999aaed715fbdaec6
tanakornsss <omtanakorn2550@gmail.com>
2025-10-14 12:38:54 +0700

    Update create journal page

--- app/src/main/java/dev/tanakornsss/luminality/ui/screen/CreateJournalScreen.kt
+++ app/src/main/java/dev/tanakornsss/luminality/ui/screen/CreateJournalScreen.kt
@@ -1,10 +1,14 @@
 package dev.tanakornsss.luminality.ui.screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -12,6 +16,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices.PIXEL_9
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,7 +28,16 @@ fun CreateJournalScreen(onNavigateBack: () -> Unit) {
     Scaffold(topBar = {
         TopAppBar(
             title = {
-                Text("Create Journal")
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text("Create Journal")
+                    Button(onClick = { }) {
+                        Text("Save")
+                    }
+                }
             },
             navigationIcon = {
                 IconButton(onClick = { onNavigateBack() }) {
