37a7e25e3f1a7c15d85317337af46ccaf1a8f071
tanakornsss <omtanakorn2550@gmail.com>
2025-10-16 09:21:17 +0700

    Add date indication

--- app/src/main/java/dev/tanakornsss/luminality/ui/screen/CreateJournalScreen.kt
+++ app/src/main/java/dev/tanakornsss/luminality/ui/screen/CreateJournalScreen.kt
@@ -3,12 +3,15 @@ package dev.tanakornsss.luminality.ui.screen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -18,13 +21,21 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Devices.PIXEL_9
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.tanakornsss.luminality.ui.theme.LuminalityTheme
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CreateJournalScreen(onNavigateBack: () -> Unit) {
+    val localDate = LocalDate.now()
+    val formattedDate = localDate
+        .format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+
     Scaffold(topBar = {
         TopAppBar(
             title = {
@@ -53,10 +64,25 @@ fun CreateJournalScreen(onNavigateBack: () -> Unit) {
     ) { innerPadding ->
         Column(modifier = Modifier
             .padding(innerPadding)
+            .padding(horizontal = 16.dp)
+            .clip(RoundedCornerShape(32.dp))
         ) {
-
+            AddJournalCard(formattedDate)
         }
     }
+}
+
+@Composable
+private fun AddJournalCard(formattedDate: String) {
+    Card(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .padding(20.dp)
+        ) {
+            Text("Date: $formattedDate")
+        }
+    }
+    Spacer(modifier = Modifier.padding(16.dp))
 }
 
 @Composable
