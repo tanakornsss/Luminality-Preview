4fa640c93296fccc8b1a10ef17ef2c6e2618e8ae
tanakornsss <omtanakorn2550@gmail.com>
2025-12-06 22:14:43 +0700

    Fix code errors

--- app/src/main/java/dev/tanakornsss/luminality/data/journal/JournalViewModelFactory.kt
+++ app/src/main/java/dev/tanakornsss/luminality/data/journal/JournalViewModelFactory.kt
@@ -4,16 +4,24 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import dev.tanakornsss.luminality.data.AppDatabase
+import dev.tanakornsss.luminality.data.streak.StreakRepository
 
 class JournalViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(JournalViewModel::class.java)) {
             val db = AppDatabase.getInstance(context)
-            val dao = db.journalDao()
-            val repo = JournalRepository(dao)
+
+            val journalDao = db.journalDao()
+            val journalRepo = JournalRepository(journalDao)
+
+            val streakDao = db.streakDao()
+            val streakRepo = StreakRepository(streakDao)
 
             @Suppress("UNCHECKED_CAST")
-            return JournalViewModel(repo) as T
+            return JournalViewModel(
+                journalRepository = journalRepo,
+                streakRepository = streakRepo
+            ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
