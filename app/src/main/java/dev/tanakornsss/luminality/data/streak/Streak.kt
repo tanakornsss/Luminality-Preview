9018e6e6ac4edaa27fb7dae32044b9f50ef4a14c
tanakornsss <omtanakorn2550@gmail.com>
2025-12-08 10:01:29 +0700

    Add basic streak table config

---
+++ app/src/main/java/dev/tanakornsss/luminality/data/streak/Streak.kt
@@ -0,0 +1,12 @@
+package dev.tanakornsss.luminality.data.streak
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "streak_table")
+data class Streak(
+    @PrimaryKey val id: Int = 1,
+    val currentStreak: Int,
+    val longestStreak: Int,
+    val lastUsedDate: Long
+)
---
+++ app/src/main/java/dev/tanakornsss/luminality/data/streak/StreakDao.kt
@@ -0,0 +1,16 @@
+package dev.tanakornsss.luminality.data.streak
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface StreakDao {
+    @Query("SELECT * FROM streak_table WHERE id = 1")
+    fun getStreak(): Flow<Streak>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun updateStreak(today: Long)
+}
