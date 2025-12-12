9d5c55c1577dc96067cb0f748fd79dc5bac6f7a5
tanakornsss <omtanakorn2550@gmail.com>
2025-12-12 11:03:41 +0700

    Fix imepadding glitch

--- app/src/main/java/dev/tanakornsss/luminality/ui/screen/NewJournalScreen.kt
+++ app/src/main/java/dev/tanakornsss/luminality/ui/screen/NewJournalScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -134,7 +135,8 @@ fun NewJournalScreen(
                     .padding(
                         vertical = 16.dp,
                         horizontal = 16.dp
-                    ),
+                    )
+                    .imePadding(),
                 value = textFieldValue,
                 onValueChange = {
                     textFieldValue = it
