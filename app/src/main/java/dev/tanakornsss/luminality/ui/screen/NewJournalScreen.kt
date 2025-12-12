6794990caefee388a4524e18088266c7c658e8c0
tanakornsss <omtanakorn2550@gmail.com>
2025-12-12 11:00:48 +0700

    Remove unused code

--- app/src/main/java/dev/tanakornsss/luminality/ui/screen/NewJournalScreen.kt
+++ app/src/main/java/dev/tanakornsss/luminality/ui/screen/NewJournalScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -84,7 +83,6 @@ fun NewJournalScreen(
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-        contentWindowInsets = WindowInsets(),
         topBar = {
             TopAppBar(
                 title = {
