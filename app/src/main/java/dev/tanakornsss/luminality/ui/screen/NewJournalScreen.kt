33ce380aaed8b99f11128ba2c0db3f13284ecdef
tanakornsss <omtanakorn2550@gmail.com>
2025-12-16 20:36:12 +0700

    Add page change on add entry

--- app/src/main/java/dev/tanakornsss/luminality/ui/screen/NewJournalScreen.kt
+++ app/src/main/java/dev/tanakornsss/luminality/ui/screen/NewJournalScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -35,6 +36,7 @@ import dev.tanakornsss.luminality.data.journal.JournalViewModel
 import dev.tanakornsss.luminality.ui.component.CustomAlertDialog
 import dev.tanakornsss.luminality.ui.screen.journal.AddJournalScreen
 import dev.tanakornsss.luminality.ui.screen.journal.JournalListScreen
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -50,6 +52,8 @@ fun NewJournalScreen(
     val streak by journalViewModel.streak.collectAsState()
 
     val pagerState = rememberPagerState(pageCount = { 2 })
+
+    val scope = rememberCoroutineScope()
 
     val exportLauncher = rememberLauncherForActivityResult(
         ActivityResultContracts.CreateDocument("application/json")
@@ -137,6 +141,9 @@ fun NewJournalScreen(
                     addEntry = {
                         journalViewModel.addEntry(textFieldValue)
                         textFieldValue = ""
+                        scope.launch {
+                            pagerState.animateScrollToPage(1)
+                        }
                     }
                 )
                 1 -> JournalListScreen(
