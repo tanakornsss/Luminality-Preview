376aa2a2c8123d42b224fa8213ba00ca312f63bc
tanakornsss <omtanakorn2550@gmail.com>
2025-12-23 21:17:10 +0700

    Change version name and number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "dev.tanakornsss.luminality"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "prerelease-0.0.2"
+        versionCode = 3
+        versionName = "prerelease-0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
