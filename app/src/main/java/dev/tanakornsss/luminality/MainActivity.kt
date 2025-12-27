76aa5de981b59c91fe65d577b86099a43b694408
tanakornsss <omtanakorn2550@gmail.com>
2025-12-27 12:43:01 +0700

    Remove unused code

--- app/src/main/java/dev/tanakornsss/luminality/MainActivity.kt
+++ app/src/main/java/dev/tanakornsss/luminality/MainActivity.kt
@@ -6,24 +6,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
-import com.google.firebase.analytics.FirebaseAnalytics
-
-lateinit var firebaseAnalytics: FirebaseAnalytics
 
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
-
-        val apiLevel = Build.VERSION.SDK_INT
-
-        firebaseAnalytics.setUserProperty(
-            "api_level",
-            apiLevel.toString()
-        )
-
         enableEdgeToEdge()
         setContent {
             LuminalityApp(this)
