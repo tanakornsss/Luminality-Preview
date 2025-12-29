b0af6fda91dd7166f9a9a0d56e46787b7f23765d
tanakornsss <omtanakorn2550@gmail.com>
2025-12-29 09:18:45 +0700

    Fix var naming

--- app/src/main/java/dev/tanakornsss/luminality/launch/LaunchRepository.kt
+++ app/src/main/java/dev/tanakornsss/luminality/launch/LaunchRepository.kt
@@ -15,45 +15,45 @@ class LaunchRepository(private val context: Context) {
     // There will be a tooltip whenever the app updates or the app was newly installed
 
     object LaunchPrefs {
-        val FIRST_LAUNCH_INSTALL = booleanPreferencesKey("first_launch_install_state")
-        val FIRST_LAUNCH_UPDATE = booleanPreferencesKey("first_launch_update_state")
-        val LAST_VERSION_NAME = stringPreferencesKey("last_ver_name_state")
+        val IS_FIRST_LAUNCH = booleanPreferencesKey("is_first_launch")
+        val IS_FIRST_LAUNCH_AFTER_UPDATE = booleanPreferencesKey("is_first_launch_after_update")
+        val LAST_LAUNCHED_VERSION = stringPreferencesKey("last_launched_version")
     }
 
 
-    suspend fun updateFirstLaunchState() {
+    suspend fun markFirstLaunchHandled() {
         context.launchDatastore.edit { state ->
-            state[LaunchPrefs.FIRST_LAUNCH_INSTALL] = true
+            state[LaunchPrefs.IS_FIRST_LAUNCH] = false
         }
     }
 
     fun readFirstLaunchState() : Flow<Boolean> {
         return context.launchDatastore.data.map { state ->
-            state[LaunchPrefs.FIRST_LAUNCH_INSTALL] ?: false
+            state[LaunchPrefs.IS_FIRST_LAUNCH] ?: true
         }
     }
 
-    suspend fun updateUpdateLaunchState() {
+    suspend fun markUpdateLaunchHandled() {
         context.launchDatastore.edit { state ->
-            state[LaunchPrefs.FIRST_LAUNCH_UPDATE] = true
+            state[LaunchPrefs.IS_FIRST_LAUNCH_AFTER_UPDATE] = false
         }
     }
 
     fun readUpdateLaunchState() : Flow<Boolean> {
         return context.launchDatastore.data.map { state ->
-            state[LaunchPrefs.FIRST_LAUNCH_UPDATE] ?: false
+            state[LaunchPrefs.IS_FIRST_LAUNCH_AFTER_UPDATE] ?: true
         }
     }
 
     suspend fun updateLastVersionName(name: String) {
         context.launchDatastore.edit { prefs ->
-            prefs[LaunchPrefs.LAST_VERSION_NAME] = name
+            prefs[LaunchPrefs.LAST_LAUNCHED_VERSION] = name
         }
     }
 
     fun readLastVersionName() : Flow<String> {
         return context.launchDatastore.data.map { prefs ->
-            prefs[LaunchPrefs.LAST_VERSION_NAME] ?: ""
+            prefs[LaunchPrefs.LAST_LAUNCHED_VERSION] ?: ""
         }
     }
 }
