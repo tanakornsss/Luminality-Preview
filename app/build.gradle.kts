ea286d462ea82ad2f29490d4b1dd4fbb6e4e4652
tanakornsss <omtanakorn2550@gmail.com>
2026-01-04 20:24:30 +0700

    Upgrade app version to prerelease-0.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "dev.tanakornsss.luminality"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "prerelease-0.0.3"
+        versionCode = 4
+        versionName = "prerelease-0.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
