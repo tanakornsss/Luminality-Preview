78679a5bf20238971dd7b11db23f17424079fcb2
tanakornsss <omtanakorn2550@gmail.com>
2026-01-16 13:04:22 +0700

    Add navigation animation

--- app/src/main/java/dev/tanakornsss/luminality/LuminalityApp.kt
+++ app/src/main/java/dev/tanakornsss/luminality/LuminalityApp.kt
@@ -1,16 +1,26 @@
 package dev.tanakornsss.luminality
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Build
 import android.util.Log
+import androidx.activity.compose.BackHandler
 import androidx.annotation.RequiresApi
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.crashlytics.FirebaseCrashlytics
@@ -31,6 +41,7 @@ import dev.tanakornsss.luminality.ui.screen.intoduction.JournalScreen
 import dev.tanakornsss.luminality.ui.screen.intoduction.OnboardScreen
 import dev.tanakornsss.luminality.ui.theme.LuminalityTheme
 
+@SuppressLint("UnusedContentLambdaTargetStateParameter")
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
 fun LuminalityApp(context: Context) {
@@ -55,6 +66,9 @@ fun LuminalityApp(context: Context) {
     }
 
     val navController = rememberNavController()
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = backStackEntry?.destination?.route
+    var navDirection by remember { mutableStateOf(NavDirection.Push) }
 
     LaunchedEffect(Unit) {
         launchViewModel.event.collect { event ->
@@ -75,46 +89,77 @@ fun LuminalityApp(context: Context) {
         }
     }
 
+    BackHandler(navController.previousBackStackEntry != null) {
+        navDirection = NavDirection.Pop
+        navController.popBackStack()
+    }
+
     LuminalityTheme {
-        NavHost(
-            navController = navController,
-            startDestination = LuminalityScreen.SPLASH.name
+        AnimatedContent(
+            targetState = currentRoute,
+            transitionSpec = {
+                if (navDirection == NavDirection.Push) {
+                    slideInHorizontally { it } togetherWith
+                            slideOutHorizontally { -it }
+                }
+                else {
+                    slideInHorizontally { -it } togetherWith
+                            slideOutHorizontally { it }
+                }
+            },
+            contentKey = { it },
+            label = "nav-animation"
         ) {
-            composable(LuminalityScreen.JOURNAL.name) {
-                JournalScreen(
-                    journalViewModel = journalViewModel,
-                    backupViewModel = backupViewModel,
-                    onNavigateSetting = {
-                        navController.navigate(LuminalityScreen.SETTING.name)
-                    }
-                )
-            }
-            composable(LuminalityScreen.SETTING.name) {
-                SettingScreen(
-                    onNavigateBack = { navController.popBackStack() },
-                    settingViewModel = settingViewModel
-                )
-            }
-            composable(LuminalityScreen.ONBOARD.name) {
-                OnboardScreen(
-                    settingViewModel = settingViewModel,
-                    onReturn = {
+            NavHost(
+                navController = navController,
+                startDestination = LuminalityScreen.SPLASH.name
+            ) {
+                composable(LuminalityScreen.JOURNAL.name) {
+                    JournalScreen(
+                        journalViewModel = journalViewModel,
+                        backupViewModel = backupViewModel,
+                        onNavigateSetting = {
+                            navDirection = NavDirection.Push
+                            navController.navigate(LuminalityScreen.SETTING.name)
+                        }
+                    )
+                }
+                composable(LuminalityScreen.SETTING.name) {
+                    SettingScreen(
+                        onNavigateBack = {
+                            navDirection = NavDirection.Pop
+                            navController.popBackStack()
+                        },
+                        settingViewModel = settingViewModel
+                    )
+                }
+                composable(LuminalityScreen.ONBOARD.name) {
+                    OnboardScreen(
+                        settingViewModel = settingViewModel,
+                        onReturn = {
+                            navDirection = NavDirection.Push
+                            navController.navigate(LuminalityScreen.JOURNAL.name) {
+                                popUpTo(0)
+                            }
+                        }
+                    )
+                }
+                composable(LuminalityScreen.WHATS_NEW.name) {
+                    WhatsNewScreen {
+                        navDirection = NavDirection.Push
                         navController.navigate(LuminalityScreen.JOURNAL.name) {
                             popUpTo(0)
                         }
                     }
-                )
-            }
-            composable(LuminalityScreen.WHATS_NEW.name) {
-                WhatsNewScreen {
-                    navController.navigate(LuminalityScreen.JOURNAL.name) {
-                        popUpTo(0)
-                    }
                 }
-            }
-            composable(LuminalityScreen.SPLASH.name) {
-                SplashScreen()
+                composable(LuminalityScreen.SPLASH.name) {
+                    SplashScreen()
+                }
             }
         }
     }
+}
+
+enum class NavDirection {
+    Push, Pop
 }
