362953ebc08d3e15bbf975289f6b1b16648ef1d6
tanakornsss <omtanakorn2550@gmail.com>
2026-06-22 10:38:07 +0700

    Add spacing in dependencies to make it more lookable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,17 +75,22 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
-    implementation(libs.kotlinx.serialization.json)
-    implementation(libs.accompanist.permissions)
     implementation(libs.androidx.navigation.compose)
-    implementation(libs.androidx.datastore.preferences)
+
+    implementation(libs.kotlinx.serialization.json)
     implementation(libs.gson)
+
+    implementation(libs.accompanist.permissions)
+
+    implementation(libs.androidx.datastore.preferences)
     implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
+
     coreLibraryDesugaring(libs.desugar.jdk.libs)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
