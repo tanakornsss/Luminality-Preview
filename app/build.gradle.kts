93d3005f4db1b32bfa3877d1f502917f45c84381
tanakornsss <omtanakorn2550@gmail.com>
2026-06-24 10:00:00 +0700

    Update app ver

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dev.tanakornsss.luminality.preview"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "preview-0.0.1"
+        versionCode = 2
+        versionName = "preview-0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
